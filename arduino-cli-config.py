b8c61f7e9515bd0f94b1bf23941266d86f00452c
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2021-10-31 23:59:07 +0100

    add option --arduino-cli

--- arduino-cli-config.py
+++ arduino-cli-config.py
@@ -12,7 +12,8 @@ LOGGER = logging.getLogger()
 LOGGER.setLevel("DEBUG")
 
 
-CLI_CMD = "/usr/src/app/bin/arduino-cli"
+DEFAULT_CLI_DIR = "/usr/src/app/bin"
+DEFAULT_CLI_CMD = f"{DEFAULT_CLI_DIR}/arduino-cli"
 CONFIG_FILE_NAME = getenv("CONFIG_FILE_NAME", "arduino-builder-config.yml")
 BUILD_DIR = "/usr/src/app/build"
 BUILD_CACHE_DIR = "/usr/src/app/build_cache"
@@ -37,24 +38,24 @@ def call(commands: List[str]):
     return check_output(commands, encoding="utf-8")
 
 
-def config_init():
-    call([CLI_CMD, "config", "init", "--overwrite"])
+def config_init(cmd):
+    call([cmd, "config", "init", "--overwrite"])
 
 
-def update_index():
-    call([CLI_CMD, "core", "update-index"])
+def update_index(cmd):
+    call([cmd, "core", "update-index"])
 
 
-def update():
-    call([CLI_CMD, "update"])
+def update(cmd):
+    call([cmd, "update"])
 
 
-def upgrade():
-    call([CLI_CMD, "upgrade"])
+def upgrade(cmd):
+    call([cmd, "upgrade"])
 
 
-def build(compile: ArduinoBuilderCompileConfig):
-    compile_cmd = [CLI_CMD, "compile", "--warnings", compile.warnings]
+def build(cmd, compile: ArduinoBuilderCompileConfig):
+    compile_cmd = [cmd, "compile", "--warnings", compile.warnings]
     if compile.verbose:
         compile_cmd.append("-v")
     compile_cmd.extend(["--build-path", BUILD_DIR, "--build-cache-path",
@@ -63,34 +64,35 @@ def build(compile: ArduinoBuilderCompileConfig):
     LOGGER.info(output)
 
 
-def install_libs(libs: List[str]):
+def install_libs(cmd, libs: List[str]):
     for lib in libs:
         LOGGER.info("Installing lib: %s", lib)
         try:
-            call([CLI_CMD, "lib", "install", lib])
+            call([cmd, "lib", "install", lib])
         except Exception as exc:
             LOGGER.exception(exc)
 
 
-def install_boards(boards: List[str]):
+def install_boards(cmd, boards: List[str]):
     for board in boards:
         LOGGER.info("Installing board: %s", board)
         try:
-            call([CLI_CMD, "core", "install", board])
+            call([cmd, "core", "install", board])
         except Exception as exc:
             LOGGER.exception(exc)
 
 
-def add_urls(urls: List[HttpUrl]):
+def add_urls(cmd, urls: List[HttpUrl]):
     for url in urls:
         LOGGER.info("Adding URL: %s", url)
-        call([CLI_CMD, "config", "add", "board_manager.additional_urls", url])
+        call([cmd, "config", "add", "board_manager.additional_urls", url])
 
 
 @click.command()
+@click.option("--arduino-cli", default=DEFAULT_CLI_CMD)
 @click.option("--compile/--no-compile", default=True)
 @click.option("--init/--no-init", default=True)
-def main(compile, init):
+def main(arduino_cli, compile, init):
     with open(CONFIG_FILE_NAME, "r") as file:
         try:
             config_yaml = yaml.safe_load(file)
@@ -101,16 +103,16 @@ def main(compile, init):
             return
 
     if init:
-        config_init()
-        add_urls(config.additional_board_urls)
-        update_index()
-        update()
-        upgrade()
-        install_libs(config.libraries)
-        install_boards(config.boards)
+        config_init(arduino_cli)
+        add_urls(arduino_cli, config.additional_board_urls)
+        update_index(arduino_cli)
+        update(arduino_cli)
+        upgrade(arduino_cli)
+        install_libs(arduino_cli, config.libraries)
+        install_boards(arduino_cli, config.boards)
 
     if config.compile and compile:
-        build(config.compile)
+        build(arduino_cli, config.compile)
 
 
 if __name__ == "__main__":
