7bb5bae95ac85dc45f32bfa6fada9d9cc32fc0a2
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2021-10-31 15:09:44 +0100

    initial commit

---
+++ arduino-cli-config.py
@@ -0,0 +1,94 @@
+from subprocess import check_output, CalledProcessError, STDOUT
+from typing import List, Optional
+from pydantic import BaseModel, HttpUrl
+import yaml
+import logging
+
+
+logging.basicConfig()
+LOGGER = logging.getLogger()
+LOGGER.setLevel("DEBUG")
+
+
+CLI_CMD = "arduino-cli"
+
+CONFIG_EXISTS_MSG = "Config file already exists, use --overwrite to discard the existing one."
+
+
+class ArduinoBuilderConfig(BaseModel):
+    additional_board_urls: Optional[List[HttpUrl]]
+    boards: Optional[List[str]]
+    libraries: Optional[List[str]]
+
+
+def call(commands: List[str], ignored_errors: List[str] = []):
+    try:
+        check_output(commands, stderr=STDOUT, encoding="utf-8")
+    except CalledProcessError as exc:
+        LOGGER.debug("EXC: %s", exc.output)
+        if any(ignored_error in exc.output for ignored_error in ignored_errors):
+            return
+        raise
+
+
+def config_init():
+    call([CLI_CMD, "config", "init", "--overwrite"], [CONFIG_EXISTS_MSG])
+
+
+def update_index():
+    call([CLI_CMD, "core", "update-index"])
+
+
+def update():
+    call([CLI_CMD, "update"])
+
+
+def upgrade():
+    call([CLI_CMD, "upgrade"])
+
+
+def install_libs(libs: List[str]):
+    for lib in libs:
+        LOGGER.info("Installing lib: %s", lib)
+        try:
+            call([CLI_CMD, "lib", "install", lib])
+        except Exception as exc:
+            LOGGER.exception(exc)
+
+
+def install_boards(boards: List[str]):
+    for board in boards:
+        LOGGER.info("Installing board: %s", board)
+        try:
+            call([CLI_CMD, "core", "install", board])
+        except Exception as exc:
+            LOGGER.exception(exc)
+
+
+def add_urls(urls: List[HttpUrl]):
+    for url in urls:
+        LOGGER.info("Adding URL: %s", url)
+        call([CLI_CMD, "config", "add", "board_manager.additional_urls", url])
+
+
+def main():
+    with open("example-config.yml", "r") as file:
+        try:
+            config_yaml = yaml.safe_load(file)
+            config = ArduinoBuilderConfig(**config_yaml)
+            LOGGER.info(config)
+        except yaml.YAMLError as exc:
+            LOGGER.exception(exc)
+            return
+
+    config_init()
+    add_urls(config.additional_board_urls)
+    update_index()
+    update()
+    upgrade()
+    install_libs(config.libraries)
+    install_boards(config.boards)
+
+
+if __name__ == "__main__":
+    main()
